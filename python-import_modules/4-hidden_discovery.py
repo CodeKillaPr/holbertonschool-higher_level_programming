7e566daf90de23e9c28e7c173cff837a87a2ae67
KILLA <killa@MacBook-Pro-de-KILLA.local>
2024-05-10 22:39:46 -0400

    fix

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
+import hidden_4
+
+
+def principal():
+    for i in dir(hidden_4):
+        if not (i[0] == '_' and i[1] == '_'):
+            print(i)
+
 
 if __name__ == "__main__":
-
-    from sys import argv
-    length = len(argv)
-    if length == 1:
-        print("0 arguments.")
-    elif length == 2:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(length - 1))
-    for i in range(1, length):
-        print("{}: {}".format(i, argv[i]))
+    principal()
