3ee6df1e4f8d82910d9f87dd83edfaa1628c417e
killa <killa@MBP-de-killa.localdomain>
2024-05-16 15:33:42 -0400

    function that give you size of square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 class Square:
 
-    def __init__(self, size=0):
-        self.size = size
+    def __init__(self):
+        pass
 
 
-@property
 def area(self):
     return self.__size ** 2
