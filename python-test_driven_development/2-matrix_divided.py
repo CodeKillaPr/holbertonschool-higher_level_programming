2c8dcfa98aa7e200778a12625722b9277d02da5a
killa <killa@MBP-de-killa.localdomain>
2024-05-20 15:54:28 -0400

    divides all elements of a matrix.

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -5,26 +5,13 @@ This is the "2-matrix_divided" module.
 
 
 def matrix_divided(matrix, div):
-    """
-    Divides all elements of a matrix by a given number.
-
-    Args:
-            matrix (list): A matrix (list of lists) of integers/floats.
-            div (int or float): The number to divide the matrix elements by.
-
-    Returns:
-            list: A new matrix with the elements divided by the given number.
-
-    Raises:
-            TypeError: If the matrix is not a matrix (list of lists) of integers/floats,
-                               or if the matrix rows have different sizes.
-            TypeError: If the div is not a number.
-            ZeroDivisionError: If the div is zero.
-    """
+    """Divides all elements of a matrix by a given number."""
     if not isinstance(matrix, list) or len(matrix) == 0:
         raise TypeError(
             "matrix must be a matrix (list of lists) of integers/floats")
-    if not all(isinstance(row, list) for row in matrix) or not all(isinstance(num, (int, float)) for num in [num for row in matrix for num in row]):
+    if not all(isinstance(row, list) for row in matrix) or not \
+            all(isinstance(num, (int, float))
+                for num in [num for row in matrix for num in row]):
         raise TypeError(
             "matrix must be a matrix (list of lists) of integers/floats")
     if not all(len(row) == len(matrix[0]) for row in matrix):
