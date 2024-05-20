6f86f51bec946d21404e62bee3d1dd53990eee58
killa <killa@MacBook-Pro-de-killa.local>
2024-05-20 19:01:19 -0400

    function that prints My name is <first name> <last name>

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -5,7 +5,16 @@ This is the "3-say_my_name" module.
 
 
 def say_my_name(first_name, last_name=""):
+	"""
 
+	Args:
+		first_name (_type_): _description_
+		last_name (str, optional): _description_. Defaults to "".
+
+	Raises:
+		TypeError: _description_
+		TypeError: _description_
+	"""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
