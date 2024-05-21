95ea45311e00cf2078974c394802c90bc33bbe9a
killa <killa@MacBook-Pro-de-killa.local>
2024-05-20 20:06:55 -0400

    Module to find the max integer in a list

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Defines a text-indentation function"""
+
+
+def max_integer(list=[]):
+    """Find the biggest integer of a list."""
+    if len(list) == 0:
+        return None
+    max = list[0]
+    for i in list:
+        if i > max:
+            max = i
+    return max
