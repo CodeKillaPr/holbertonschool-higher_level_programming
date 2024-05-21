e9b34c3f83028c907a9a1a74437c73d918de3b09
killa <killa@MacBook-Pro-de-killa.local>
2024-05-20 20:10:05 -0400

    Module to find the max integer in a list

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,29 +1,26 @@
 #!/usr/bin/python3
-"""Defines a text-indentation function."""
+"""
+
+Text indentation
+
+"""
 
 
 def text_indentation(text):
-    """Print text with two new lines after each '.', '?', and ':'.
-
-    Args:
-        text (string): The text to print.
-    Raises:
-        TypeError: If text is not a string.
     """
-    if not isinstance(text, str):
-        raise TypeError("text must be a string")
+    print text with 2 new lines after each specified char
+    """
 
-    c = 0
-    while c < len(text) and text[c] == ' ':
-        c += 1
+    error1 = "text must be a string"
+    if type(text) is not str:
+        raise TypeError(error1)
 
-    while c < len(text):
-        print(text[c], end="")
-        if text[c] == "\n" or text[c] in ".?:":
-            if text[c] in ".?:":
-                print("\n")
-            c += 1
-            while c < len(text) and text[c] == ' ':
-                c += 1
+    brk = True
+    for i in text.strip():
+        if brk and i == ' ':
             continue
-        c += 1
+        print(i, end="")
+        brk = False
+        if i in ['.', '?', ':']:
+            print("\n")
+            brk = True
