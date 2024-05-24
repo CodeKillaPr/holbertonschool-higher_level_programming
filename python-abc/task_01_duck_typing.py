192063ad45c00cb60186258c6743ce26eae1772a
killa <killa@MacBook-Pro-de-killa.local>
2024-05-24 09:50:18 -0400

    Define an abstract class Shape using the ABC package

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -5,19 +5,3 @@ class Shape(ABC):
     @abstractmethod
     def area(self):
         pass
-
-
-class Circle(Shape):
-    def __init__(self, radius):
-        self.radius = radius
-
-    def area(self):
-        return 3.14 * self.radius ** 2
-
-
-class Square(Shape):
-    def __init__(self, side):
-        self.side = side
-
-    def area(self):
-        return self.side ** 2
