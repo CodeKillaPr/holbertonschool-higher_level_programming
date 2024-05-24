73a7303433842f0a830d405a72dc366ec9814b09
killa <killa@MacBook-Pro-de-killa.local>
2024-05-24 09:54:10 -0400

    abstract class named Animal using the ABC package

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,3 +1,4 @@
+import math
 from abc import ABC, abstractmethod
 
 
@@ -6,10 +7,17 @@ class Shape(ABC):
     def area(self):
         pass
 
+    @abstractmethod
+    def perimeter(self):
+        pass
+
 
 class Circle(Shape):
     def __init__(self, radius):
         self.radius = radius
 
     def area(self):
-        return 3.14 * self.radius ** 2
+        return math.pi * self.radius ** 2
+
+    def perimeter(self):
+        return 2 * math.pi * self.radius
