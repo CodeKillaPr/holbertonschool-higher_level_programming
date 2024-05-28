ad8845a2894346d0b7c96c8051ad66ee6c569edb
killa <killa@MBP-de-killa.localdomain>
2024-05-28 14:33:38 -0400

    class Student that defines a student

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -3,16 +3,24 @@
 
 
 class Student:
-    """ la madre del que se copie"""
-
     def __init__(self, first_name, last_name, age):
-        """ la madre del que se copie"""
+        """
+        Initializes a new Student instance.
+
+        :param first_name: The first name of the student.
+        :param last_name: The last name of the student.
+        :param age: The age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-        def to_json(self):
-            """ la madre del que se copie"""
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of the Student instance.
+
+        :return: A dictionary containing the student's first name, last name, and age.
+        """
         return {
             'first_name': self.first_name,
             'last_name': self.last_name,
