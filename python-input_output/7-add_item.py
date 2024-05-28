7c77af13afa235a313d44f646515aacbc19bf355
killa <killa@MBP-de-killa.localdomain>
2024-05-28 13:55:26 -0400

    script that adds all arguments to a Python list, and then save them to a file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""Script that adds all arguments
+to a Python list, and then save them to a file	"""
+
+import sys
+import os.path
+
+filename = "add_item.json"
+""" Check if the file exists, if it does,
+load the content of the file into my_list, if not, create an empty list"""
+if os.path.exists(filename):
+    with open(filename, 'r') as f:
+        my_list = json.load(f)
+else:
+    my_list = []
+
+for i in range(1, len(sys.argv)):
+    my_list.append(sys.argv[i])
+
+with open(filename, 'w') as f:
+    json.dump(my_list, f)
