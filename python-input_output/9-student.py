292cb4feed564eaab2ebb3f4a8e25e8602e54d55
killa <killa@MBP-de-killa.localdomain>
2024-05-28 14:32:42 -0400

    class Student that defines a student

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -13,4 +13,8 @@ class Student:
 
         def to_json(self):
             """ la madre del que se copie"""
-            return self.__dict__
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
