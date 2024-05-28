d0554fa997d2fd46770e4abace2fa32e83edacdd
killa <killa@MBP-de-killa.localdomain>
2024-05-28 13:31:43 -0400

    function that returns the JSON representation of an object (string):

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Module that contains a function that
+returns an object (Python data structure)"""
+
+
+def from_json_string(my_str):
+    """Function that returns an object (Python data structure)"""
+    import json
+    return json.loads(my_str)
