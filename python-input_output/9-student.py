9c62663d797b009b45e2c4ec6d02963810702fab
killa <killa@MBP-de-killa.localdomain>
2024-05-28 14:29:38 -0400

    class Student that defines a student

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+""" la madre del que se copie """
+
+
+class student:
+    """ la madre del que se copie"""
+
+    def __init__(self, first_name, last_name, age):
+        """ la madre del que se copie"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+        def to_json(self):
+            """ la madre del que se copie"""
+            return self.__dict__
