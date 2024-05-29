35faed049178ba0d9ce0e106d7f36abe2b86936c
killa <killa@MBP-de-killa.home.lla.com>
2024-05-29 10:28:54 -0400

    fixing output

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -4,7 +4,8 @@ import xml.etree.ElementTree as ET
 
 def serialize_to_xml(dictionary, filename):
     """
-    Serialize a Python dictionary into XML format and save it to the given filename.
+    Serialize a Python dictionary into XML
+    format and save it to the given filename.
 
     Parameters:
     dictionary (dict): The dictionary to serialize.
