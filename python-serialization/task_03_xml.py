c37124d056e91bfac8c23ba964f6af67f1e2deee
killa <killa@MBP-de-killa.home.lla.com>
2024-05-29 10:39:47 -0400

    fixing output

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -1,16 +1,22 @@
 import xml.etree.ElementTree as ET
 
+""" Serialize a Python dictionary into XML """
+
 
 def serialize_to_xml(dictionary, filename):
-    """ Create the root element of the XML tree """
+    """
+    Serialize a Python dictionary into XML format and save it to the given filename.
+
+    Parameters:
+    dictionary (dict): The dictionary to serialize.
+    filename (str): The filename where the XML data will be saved.
+    """
     root = ET.Element("data")
 
-    """ Iterate over the dictionary and create elements for each key-value pair """
     for key, value in dictionary.items():
         item = ET.SubElement(root, key)
         item.text = str(value)
 
-    """ Create the XML tree and write it to the file """
     tree = ET.ElementTree(root)
     try:
         tree.write(filename, encoding='utf-8', xml_declaration=True)
@@ -21,24 +27,52 @@ def serialize_to_xml(dictionary, filename):
 
 
 def deserialize_from_xml(filename):
+    """
+    Deserialize XML data from a given filename into a Python dictionary.
+
+    Parameters:
+    filename (str): The filename from which to read the XML data.
+
+    Returns:
+    dict: The deserialized dictionary.
+
+    Raises:
+    Exception: If an error occurs while reading from the file.
+    """
     try:
-        """ Parse the XML file and get the root element """
         tree = ET.parse(filename)
         root = tree.getroot()
-        dictionary = {}
+        dictionary = {child.tag: child.text for child in root}
 
-        """ Iterate over the child elements of the root element """
-        for child in root:
-            value = child.text
+        for key, value in dictionary.items():
             if value.isdigit():
-                dictionary[child.tag] = int(value)
+                dictionary[key] = int(value)
             else:
                 try:
-                    dictionary[child.tag] = float(value)
+                    dictionary[key] = float(value)
                 except ValueError:
-                    dictionary[child.tag] = value
+                    pass
 
         return dictionary
     except Exception as e:
         print(f"An error occurred while reading from the file: {e}")
         return None
+
+
+if __name__ == "__main__":
+    sample_dict = {
+        'name': 'John',
+        'age': '28',
+        'city': 'New York'
+    }
+
+    xml_file = "data.xml"
+    if serialize_to_xml(sample_dict, xml_file):
+        print(f"Dictionary serialized to {xml_file}")
+
+    deserialized_data = deserialize_from_xml(xml_file)
+    if deserialized_data:
+        print("\nDeserialized Data:")
+        print(deserialized_data)
+    else:
+        print("Failed to deserialize data from 'data.xml'.")
