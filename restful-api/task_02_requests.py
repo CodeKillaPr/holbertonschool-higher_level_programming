3ed21ae1478c30b023f3b09710a9603ac41e99d3
killa <killa@MBP-de-killa.localdomain>
2024-05-30 13:22:45 -0400

    Consuming and processing data from an API using Python

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -37,9 +37,9 @@ def fetch_and_save_posts():
         for post in structured_post:
             writer.writerow(post)
 
-        print(f"Data has been written to posts.csv successfully.")
+        print("Data has been written to posts.csv successfully.")
     else:
-        print(f"Failed to fetch posts. Status code: {url.status_code}")
+        print("Failed to fetch posts. Status code: {url.status_code}")
 
 
 fetch_and_print_posts()
