ba82d131961ebf4151f9667fe1dc0ae91991a07c
killa <killa@MacBook-Pro-de-killa.local>
2024-05-30 21:08:43 -0400

    task 03

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,10 +1,23 @@
-import http.server
 import socketserver
+import http.server
 
 PORT = 8000
 
-Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
+class CustomHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            # Handle the root endpoint
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b'Hello, World!')
+        else:
+            # Handle undefined endpoints
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(b'Endpoint not found')
+
+
+with socketserver.TCPServer(("", PORT), CustomHandler) as httpd:
     print("serving at port", PORT)
     httpd.serve_forever()
