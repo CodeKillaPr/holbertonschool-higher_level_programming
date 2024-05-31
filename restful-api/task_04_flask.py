fed33b3cd0278b6a53923a5cad8a0abb2f5c05e1
killa <killa@MacBook-Pro-de-killa.local>
2024-05-31 09:34:44 -0400

    task 04

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -6,21 +6,29 @@ app = Flask(__name__)
 users = {}
 
 # Ruta principal
+
+
 @app.route("/")
 def home():
     return "Welcome to the Flask API!"
 
 # Ruta para devolver los nombres de usuario
+
+
 @app.route("/data")
 def get_usernames():
     return jsonify(list(users.keys()))
 
 # Ruta para verificar el estado
+
+
 @app.route("/status")
 def status():
     return "OK"
 
 # Ruta para obtener detalles de un usuario
+
+
 @app.route("/users/<username>")
 def get_user(username):
     user = users.get(username)
@@ -30,6 +38,8 @@ def get_user(username):
         return jsonify({"error": "User not found"}), 404
 
 # Ruta para agregar un nuevo usuario
+
+
 @app.route("/add_user", methods=["POST"])
 def add_user():
     new_user = request.json
@@ -39,7 +49,8 @@ def add_user():
     if username in users:
         return jsonify({"error": "User already exists"}), 400
     users[username] = new_user
-    return jsonify({"message": "User added", "user": new_user}), 201
+    return jsonify(new_user), 201
+
 
 if __name__ == "__main__":
     app.run()
