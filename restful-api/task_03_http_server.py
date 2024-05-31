b8cf265a9e00f0666dd8205bd283e12261b612ba
killa <killa@MacBook-Pro-de-killa.local>
2024-05-30 22:44:05 -0400

    task 03

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -31,7 +31,7 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
-            error_message = {"error": "Endpoint not found"}
+            error_message = {"404 Not Found": "Endpoint not found"}
             self.wfile.write(json.dumps(error_message).encode())
 
 
