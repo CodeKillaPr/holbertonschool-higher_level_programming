4f08d388f5417cd18df44eb0ea6b485cc1a6d330
killa <killa@MacBook-Pro-de-killa.local>
2024-05-30 22:45:26 -0400

    task 03

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,5 +1,5 @@
-import json
 import http.server
+import json
 
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
@@ -20,18 +20,11 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(b'OK')
-        elif self.path == '/info':
-            self.send_response(200)
-            self.send_header('Content-type', 'application/json')
-            self.end_headers()
-            info = {"version": "1.0",
-                    "description": "A simple API built with http.server"}
-            self.wfile.write(json.dumps(info).encode())
         else:
             self.send_response(404)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
-            error_message = {"404 Not Found": "Endpoint not found"}
+            error_message = {"error": "Endpoint not found"}
             self.wfile.write(json.dumps(error_message).encode())
 
 
