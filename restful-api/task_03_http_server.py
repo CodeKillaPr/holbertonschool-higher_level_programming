43713e3b994c441a0e78ccde3aed26810aa6778a
killa <killa@MacBook-Pro-de-killa.local>
2024-05-30 20:57:01 -0400

    task 03

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -35,11 +35,10 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
-            self.send_response(404)
-            self.send_header("Content-type", "text/plain")
-            self.end_headers()
-            self.wfile.write(
-                b"404 Not Found: The requested resource was not found on this server.")
+            error_message = {
+                "error": "Endpoint not found"
+            }
+            self.wfile.write(json.dumps(error_message).encode('utf-8'))
 
 
 def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8000):
