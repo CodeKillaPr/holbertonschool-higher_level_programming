09313cf3336184409396a79abc52f3d6f4d3808f
KILLA <nelsonba787@gmail.com>
2024-06-17 16:11:32 -0400

    full description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,2 @@
---  Full description
-SELECT column_name, data_type, character_maximum_length
-FROM information_schema.columns
-WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table';
-
+--  Full description:
+SHOW CREATE TABLE first_table;
